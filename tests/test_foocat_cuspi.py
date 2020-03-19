a7c4ab880d586eeef33da2984a091924b9f95c15
Victor Cuspinera <vcuspinera@gmail.com>
2020-03-18 18:36:20 -0700

    added unit tests for catbind

--- tests/test_foocat_cuspi.py
+++ tests/test_foocat_cuspi.py
@@ -1,1 +1,10 @@
 from foocat_cuspi import foocat_cuspi
+import pandas as pd
+
+#{pyt{python foocat-test, eval = FALSE}m foocat import foocat import pandas as pd
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert((foocat_cuspi.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert((foocat_cuspi.catbind(a, b)).categories == ["but", "character", "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
